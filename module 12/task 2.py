069aef03e7438da3d185b929cc5a7f345613dfef
Robeco945 <carettoroberto0312@gmail.com>
2024-10-30 11:23:55 +0200

    done

--- module 12/task 2.py
+++ module 12/task 2.py
@@ -1,6 +1,7 @@
 import requests
+from key import key
 name = input('enter city name: ')
-request = f"https://api.openweathermap.org/data/2.5/weather?q={name}&appid=36f80f5b6457341641e2b73c4abe0e19&units=metric"
+request = f"https://api.openweathermap.org/data/2.5/weather?q={name}&appid={key}&units=metric"
 response = requests.get(request).json()
 print(f'weather condition: {response["weather"][0]["description"]}')
 print(f'current temperature: {response["main"]["temp"]} celsius')
